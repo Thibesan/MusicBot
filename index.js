ec905f4d32d856a55ef8706c620db261e4c64e97
Thibesan Raveenderan <traveen2@uwo.ca>
2023-01-02 12:47:59 -0500

    Play Command Functionality, Updated Formatting

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-const Discord = require("discord.js");
-const dotenv = require("dotenv");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("@discord-api-types/v9");
-const fs = require("fs");
-const { Player } = require("discord-player");
+const Discord = require('discord.js')
+const dotenv = require('dotenv')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('@discord-api-types/v9')
+const fs = require('fs')
+const { Player } = require('discord-player')
 
-dotenv.config();
-const TOKEN = process.env.TOKEN; //Bot Token ID, unique
+dotenv.config()
+const TOKEN = process.env.TOKEN //Bot Token ID, unique
 
 //Initialize commands given load command, else compile normally
-const LOAD_SLASH = process.argv[2] == "load";
+const LOAD_SLASH = process.argv[2] == 'load'
 
-const CLIENT_ID = "1058066067366367232";
-const GUILD_ID = "1058067278379368528";
+const CLIENT_ID = '1058066067366367232'
+const GUILD_ID = '1058067278379368528'
 
 const client = new Discord.Client({
-  intents: ["GUILDS", "GUILDS_VOICE_STATES"],
-});
+	intents: ['GUILDS', 'GUILDS_VOICE_STATES'],
+})
 
-client.slashcommands = new Discord.Collection();
+client.slashcommands = new Discord.Collection()
 client.player = new Player(client, {
-  ytdlOptions: {
-    quality: "highestaudio",
-    highWaterMark: 1 << 25,
-  },
-});
+	ytdlOptions: {
+		quality: 'highestaudio',
+		highWaterMark: 1 << 25,
+	},
+})
 
-let commands = [];
+let commands = []
 const slashFiles = fs
-  .readdirSync("./slashCommands")
-  .filter((file) => file.endsWith(".js"));
+	.readdirSync('./slashCommands')
+	.filter((file) => file.endsWith('.js'))
 
 for (const file of slashFiles) {
-  const slashcmd = require("./slashCommands/${file}");
-  client.slashcommands.set(slashcmd.data.name, slashcmd);
-  if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
+	const slashcmd = require('./slashCommands/${file}')
+	client.slashcommands.set(slashcmd.data.name, slashcmd)
+	if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
 }
 
 if (LOAD_SLASH) {
-  const rest = new REST({ version: "9" }).setToken(TOKEN);
-  console.log("Deploying Slash Commands");
-  rest
-    .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    })
-    .then(() => {
-      console.log("Successfully Loaded");
-      process.exit(0);
-    })
-    .catch((err) => {
-      if (err) {
-        console.log(err);
-        process.exit(1);
-      }
-    });
+	const rest = new REST({ version: '9' }).setToken(TOKEN)
+	console.log('Deploying Slash Commands')
+	rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+		body: commands,
+	})
+		.then(() => {
+			console.log('Successfully Loaded')
+			process.exit(0)
+		})
+		.catch((err) => {
+			if (err) {
+				console.log(err)
+				process.exit(1)
+			}
+		})
 } else {
-  client.on("ready", () => {
-    console.log("Logged in as ${client.user.tag}");
-  });
-  client.on("interactionCreate", (interaction) => {
-    async function handleCommand() {
-      if (!interaction.isCommand()) return;
+	client.on('ready', () => {
+		console.log(`Logged in as ${client.user.tag}`)
+	})
+	client.on('interactionCreate', (interaction) => {
+		async function handleCommand() {
+			if (!interaction.isCommand()) return
 
-      const slashcmd = client.slashcommands.get(interaction.commandName);
-      if (!slashcmd) interaction.reply("Not a valid slash command");
+			const slashcmd = client.slashcommands.get(
+				interaction.commandName
+			)
+			if (!slashcmd) interaction.reply('Not a valid slash command')
 
-      await interaction.deferReply();
-      await slashcmd.run({ client, interaction });
-    }
-    handleCommand();
-  });
-  client.login(TOKEN);
+			await interaction.deferReply()
+			await slashcmd.run({ client, interaction })
+		}
+		handleCommand()
+	})
+	client.login(TOKEN)
 }
--- slashCommands/play.js
+++ slashCommands/play.js
@@ -1,59 +1,120 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
-const { QueryType } = require("discord-player");
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
+const { QueryType } = require('discord-player')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("Loads Songs from YT")
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('Loads Songs from YT')
 
-    .addSubcommand((subcommand) => {
-      subcommand
-        .setName("song")
-        .setDescription("Loads Single Song from URL")
-        .addStringOption((option) => {
-          option.setName("url").setDescription("Song URL").setRequired(true);
-        });
-    })
+		.addSubcommand((subcommand) => {
+			subcommand
+				.setName('song')
+				.setDescription('Loads Single Song from URL')
+				.addStringOption((option) => {
+					option
+						.setName('url')
+						.setDescription('Song URL')
+						.setRequired(true)
+				})
+		})
 
-    .addSubcommand((subcommand) => {
-      subcommand
-        .setName("playlist")
-        .setDescription("Loads Playlist Songs from URL")
-        .addStringOption((option) => {
-          option
-            .setName("url")
-            .setDescription("Playlist URL")
-            .setRequired(true);
-        });
-    })
+		.addSubcommand((subcommand) => {
+			subcommand
+				.setName('playlist')
+				.setDescription('Loads Playlist Songs from URL')
+				.addStringOption((option) => {
+					option
+						.setName('url')
+						.setDescription('Playlist URL')
+						.setRequired(true)
+				})
+		})
 
-    .addSubcommand((subcommand) => {
-      subcommand
-        .setName("search")
-        .setDescription("Search for Song based on Keywords")
-        .addStringOption((option) => {
-          option
-            .setName("searchTerms")
-            .setDescription("Search Keywords")
-            .setRequired(true);
-        });
-    }),
+		.addSubcommand((subcommand) => {
+			subcommand
+				.setName('search')
+				.setDescription('Search for Song based on Keywords')
+				.addStringOption((option) => {
+					option
+						.setName('searchTerms')
+						.setDescription('Search Keywords')
+						.setRequired(true)
+				})
+		}),
 
-  run: async ({ client, interaction }) => {
-    if (!interaction.member.voice.channel)
-      return interaction.editReply("Must be in VC to use this command");
+	run: async ({ client, interaction }) => {
+		if (!interaction.member.voice.channel)
+			return interaction.editReply('Must be in VC to use this command')
 
-    const queue = await client.player.createQueue(interaction.guild);
-    if (!queue.connection)
-      await queue.connect(interaction.member.voice.channel);
+		const queue = await client.player.createQueue(interaction.guild)
+		if (!queue.connection)
+			await queue.connect(interaction.member.voice.channel)
 
-    let embed = new MessageEmbed();
+		let embed = new MessageEmbed()
 
-    //Single Song Handler
+		//Song Handler
+		if (interaction.options.getSubcommand() === 'song') {
+			let url = interaction.options.getString('url')
+			const result = await client.player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.YOUTUBE_VIDEO,
+			})
 
-    //Playlist Handler
+			if (result.tracks.length === 0) {
+				return interaction.editReply('No Results')
+			}
 
-    //Search Handler
-  },
-};
+			const song = result.tracks[0]
+			await queue.addTrack(song)
+			embed
+				.setDescription(`||${song.title} - ${song.url}|| has been added to the Queue||`)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}` })
+				
+		//Playlist Handler
+		} else if (interaction.options.getSubcommand() === 'playlist') {
+			let url = interaction.options.getString('url')
+			const result = await client.player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.YOUTUBE_PLAYLIST,
+			})
+
+			if (result.tracks.length === 0) {
+				return interaction.editReply('No Results')
+			}
+			
+			const playlist = result.playlist
+			await queue.addTracks(result.tracks)
+			embed
+				.setDescription(`||${result.tracks.length} songs from ${playlist.title} - ${playlist.url}|| have been added to the Queue||`)
+				.setThumbnail(playlist.thumbnail)
+				.setFooter({ text: `Duration: ${playlist.duration}` })
+				
+		//Search Handler
+		} else if (interaction.options.getSubcommand() === 'search') {
+			let url = interaction.options.getString('searchTerms')
+			const result = await client.player.search(url, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.AUTO,
+			})
+
+			if (result.tracks.length === 0) {
+				return interaction.editReply('No Results')
+			}
+
+			const song = result.tracks[0]
+			await queue.addTrack(song)
+			embed
+				.setDescription(`||${song.title} - ${song.url}|| has been added to the Queue||`)
+				.setThumbnail(song.thumbnail)
+				.setFooter({ text: `Duration: ${song.duration}` })
+		}
+		//Force Play State if Queue Modified
+		if (!queue.playing)
+			await queue.play()
+		await interaction.editReply({
+			embeds: [embed]
+		})
+	}
+}
